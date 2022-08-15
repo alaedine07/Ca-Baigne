da46dabf15cfb659de35a1f4d0b4050bfcd5ce85
alaedine07 <alaedineboufaden@gmail.com>
2022-08-15 16:52:45 +0100

    configuring react app with webpack and babel

---
+++ Frontend/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function App() {
+    return (
+        <div>
+            <h1>Hello React app!</h1>
+        </div>
+    );
+}
+
+export default App
---
+++ Frontend/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index.bundle.js',
+    },
+    devServer: {
+        port: 3010
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+};
