9107240e09246f35ec5570c3487c97a1567cbdaf
alaeddine <boufaden.alaeddine.dev@gmail.com>
2022-08-22 18:15:28 +0100

    False is false

--- Frontend/src/components/App.jsx
+++ Frontend/src/components/App.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
+import { useState } from "react";
 
 import SignInForm from './Auth/sign_in';
 import SignUpForm from "./Auth/sign_up";
 import Home from './Home/Home';
 import Header from "./Header/Header";
 
-
 import './App.css';
 
 export function App() {
     const location = useLocation();
+    const [ isLoggedIn, setIsLoggedIn ] = useState(false);
     return (
         <React.StrictMode>
         <div>
