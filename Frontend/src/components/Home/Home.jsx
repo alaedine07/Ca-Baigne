16460495f5c7e8b3a31f20e2d05d29032b8be268
alaeddine <boufaden.alaeddine.dev@gmail.com>
2022-08-25 13:12:50 +0100

    remove comment

--- Frontend/src/components/Home/Home.jsx
+++ Frontend/src/components/Home/Home.jsx
@@ -7,8 +7,6 @@ import './Home.css'
 export function Home() {
     return (
         <div className="container">
-            {/* <Link to="login"> Click here to login </Link>
-            <Link to="join"> Click here to join   </Link> */}
             <SearchBox />
         </div>
     )
