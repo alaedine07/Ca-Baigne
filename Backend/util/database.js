b896b1fbf299dfb0dc3ed82d5421cb6b51d45dcd
alaeddine <boufaden.alaeddine.dev@gmail.com>
2022-09-09 09:57:45 +0100

    adding dockerfiles and production database

--- Backend/util/database.js
+++ Backend/util/database.js
@@ -3,7 +3,10 @@ const Sequelize = require('sequelize').Sequelize;
 // loads environment variables from a .env file into the process.env object
 require('dotenv').config()
 
-const db = new Sequelize(
+var db;
+
+if (process.env.NODE_ENV === "test") {
+    db = new Sequelize(
     process.env.DATABASE,
     process.env.DATABASE_USER,
     process.env.DATABASE_PASSWORD,
@@ -12,6 +15,24 @@ const db = new Sequelize(
         timezone: '+01',
         logging: false
     },
-)
+)}
+else {
+    db = new Sequelize(
+    process.env.PROD_DATABASE_NAME,
+    process.env.PROD_DATABASE_USER,
+    process.env.PROD_DATABASE_PASSWORD,
+    {
+        host: process.env.PROD_DATABASE_HOST,
+        dialect: 'postgres',
+        timezone: '+01',
+        logging: false,
+        ssl: true,
+        dialectOptions: {
+            ssl: {
+                require: true
+            }
+        }
+    }
+)}
 
 module.exports = db
